d0ac2df9ba26e453f27b2ba93d1e6dbc24677c7f
Jessica Bates <chamus55@yahoo.com>
2024-08-26 20:03:39 -0700

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World')
+console.log("Hello from inside the main.js file");
+
+//let myVar;
+
+const myVar = "Jessica";
+const myVarType = typeof(myVar);
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+
+
+function runNow () {
+    if(myVarType === "number") {
+        console.log(`will 11 this one run?`)
+        } else {
+            console.log(`will 13 this one run?`);
+        }
+}
+
+runNow();
+runNow();
